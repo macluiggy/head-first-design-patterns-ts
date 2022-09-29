10cf0388917aa853de801eea2d4e9c9ec6b2e5a9
luiggy macias <ferrinluiggy@gmail.com>
2022-09-28 19:54:20 -0500

    se agregan nuevos metodos

--- src/factory/pizzaaf/NYPizzaIngredientFactory.ts
+++ src/factory/pizzaaf/NYPizzaIngredientFactory.ts
@@ -1,13 +1,17 @@
 import Cheese from "./Cheese";
+import Clams from "./Clams";
 import Dough from "./Dough";
+import FreshClams from "./FreshClams";
 import Garlic from "./Garlic";
 import MarinaraSauce from "./MarinaraSauce";
 import Mushroom from "./Mushroom";
 import Onion from "./Onion";
+import Pepperoni from "./Pepperoni";
 import PizzaIngredientFactory from "./PizzaIngredientFactory";
 import RedPepper from "./RedPepper";
 import ReggianoCheese from "./ReggianoCheese";
 import Sauce from "./Sauce";
+import SlicedPepperoni from "./SlicedPepperoni";
 import ThinCrustDough from "./ThinCrustDough";
 import Veggies from "./Veggies";
 
@@ -28,5 +32,13 @@ export default class NYPizzaIngredientFactory
 
   createVeggies(): Veggies[] {
     return [new Garlic(), new Onion(), new Mushroom(), new RedPepper()];
+  }
+
+  createPepperoni(): Pepperoni {
+    return new SlicedPepperoni()
+  }
+
+  createClam(): Clams {
+    return new FreshClams();
   }
 }
--- src/factory/pizzaaf/PizzaIngredientFactory.ts
+++ src/factory/pizzaaf/PizzaIngredientFactory.ts
@@ -2,12 +2,14 @@ import Dough from "./Dough";
 import Sauce from "./Sauce";
 import Cheese from "./Cheese";
 import Veggies from "./Veggies.js";
+import Pepperoni from "./Pepperoni";
+import Clams from "./Clams";
 
 export default interface PizzaIngredientFactory {
   createDough(): Dough;
   createSauce(): Sauce;
   createCheese(): Cheese;
   createVeggies(): Veggies[];
-  // createPepperoni(): Pepperoni;
-  // createClam(): Clams;
+  createPepperoni(): Pepperoni;
+  createClam(): Clams;
 }
