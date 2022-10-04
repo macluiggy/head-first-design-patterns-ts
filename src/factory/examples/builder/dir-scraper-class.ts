0a15ce4cece785be87b53d440065619a03811c57
luiggy macias <ferrinluiggy@gmail.com>
2022-10-03 21:37:00 -0500

    ggg

---
+++ src/factory/examples/builder/dir-scraper-class.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+
+interface IFileReader {
+  isJsonFile: (path: string) => boolean;
+  readText(file: string): string;
+  readJson(file: string): unknown;
+}
+
+class DirectoryScraper {
+  constructor(public dirPath: string, public fileReader: IFileReader) {}
+
+  scanFiles() {
+    return fs
+      .readdirSync(this.dirPath)
+      .reduce<Record<string, unknown>>((acc, file) => {
+        if (this.fileReader.isJsonFile(file)) {
+          acc[file] = this.fileReader.readJson(`${this.dirPath}/${file}`);
+        } else {
+          acc[file] = this.fileReader.readText(`${this.dirPath}/${file}`);
+        }
+        return acc;
+      }, {});
+  }
+}
+
+class FileReader implements IFileReader {
+  isJsonFile(path: string) {
+    return path.endsWith(".json");
+  }
+
+  readText(file: string) {
+    return fs.readFileSync(file, "utf-8").toString();
+  }
+
+  readJson(file: string) {
+    return JSON.parse(this.readText(file));
+  }
+}
+
+const fileReader = new FileReader();
+const dirScraper = new DirectoryScraper("./data", fileReader);
+const output = dirScraper.scanFiles();
+console.log(output);
+
