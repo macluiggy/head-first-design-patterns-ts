a9c91ca7532d476389c6fb4f9b8af2b898728c7a
luiggy macias <ferrinluiggy@gmail.com>
2023-08-01 21:09:49 -0500

    execute with ts node

--- src/command/simpleremote/SimpleRemoteControl.ts
+++ src/command/simpleremote/SimpleRemoteControl.ts
@@ -1,12 +1,16 @@
 import Command from "./Command";
 export default class SimpleRemoteControl {
-  private slot: Command;
+  private slot: Command | null;
+
+  constructor() {
+    this.slot = null
+  }
 
   public setCommand(command: Command): void {
     this.slot = command;
   }
 
   public buttonWasPressed(): void {
-    this.slot.execute();
+    this.slot!.execute();
   }
 }
