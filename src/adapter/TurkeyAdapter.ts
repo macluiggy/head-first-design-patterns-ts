357dbf93796ec2765b0f72cc544486347093ba13
macluiggy <luiggy.macias@imagineapps.co>
2024-02-16 11:39:50 -0500

    Add TurkeyAdapter class

---
+++ src/adapter/TurkeyAdapter.ts
@@ -0,0 +1,19 @@
+import Duck from "./Duck";
+import Turkey from "./Turkey";
+
+export default class TurkeyAdapter implements Duck {
+  private turkey: Turkey;
+  constructor(turkey: Turkey) {
+    this.turkey = turkey;
+  }
+
+  quack(): void {
+    this.turkey.gobble();
+  }
+
+  fly(): void {
+    for (let i = 0; i < 5; i++) {
+      this.turkey.fly();
+    }
+  }
+}
