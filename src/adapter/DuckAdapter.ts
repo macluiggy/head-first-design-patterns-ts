3d8c2cc61885cd81e07282af7333fbe9435aaad7
macluiggy <luiggy.macias@imagineapps.co>
2024-02-16 12:13:35 -0500

    Fix conditional fly behavior in DuckAdapter

--- src/adapter/DuckAdapter.ts
+++ src/adapter/DuckAdapter.ts
@@ -3,12 +3,16 @@ import Turkey from "./Turkey";
 
 export default class DuckAdapter implements Turkey {
   private duck: Duck;
+  private rand
   constructor(duck: Duck) {
     this.duck = duck;
+    this.rand = Math.floor(Math.random() * 5);
   }
 
   fly(): void {
-    this.duck.fly();
+    if (this.rand === 0) {
+      this.duck.fly();
+    }
   }
   gobble(): void {
     this.duck.quack();
