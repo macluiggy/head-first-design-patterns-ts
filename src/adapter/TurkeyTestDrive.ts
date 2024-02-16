2805556993bd1d8a0d27df92594723e6705061bc
macluiggy <luiggy.macias@imagineapps.co>
2024-02-16 11:52:47 -0500

    Add TurkeyTestDrive class to test the TurkeyAdapter

---
+++ src/adapter/TurkeyTestDrive.ts
@@ -0,0 +1,31 @@
+import Duck from "./Duck";
+import DuckAdapter from "./DuckAdapter";
+import MallardDuck from "./MallardDuck";
+import Turkey from "./Turkey";
+import TurkeyAdapter from "./TurkeyAdapter";
+import WildTurkey from "./WildTurkey";
+
+export default class TurkeyTestDrive {
+  static main(): void {
+    const duck = new MallardDuck();
+    const turkey = new WildTurkey();
+    const duckAdapter = new DuckAdapter(duck);
+
+    console.log('The Duck says...');
+    duck.quack();
+    duck.fly();
+
+    console.log('\nThe Turkey says...');
+    TurkeyTestDrive.testTurkey(turkey);
+
+    console.log('\nThe TurkeyAdapter says...');
+    TurkeyTestDrive.testTurkey(duckAdapter);
+  }
+
+  static testTurkey(duck: Turkey): void {
+    duck.gobble();
+    duck.fly();
+  }
+}
+
+TurkeyTestDrive.main();
