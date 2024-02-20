d49de3b030a42ca37fecaea90a66b450c6bf5d95
macluiggy <luiggy.macias@imagineapps.co>
2024-02-20 14:03:49 -0500

    Add CaffeineBeverageWithHook class for template method pattern implementation

---
+++ src/templatemethod/barista/CaffeineBeverageWithHook.ts
@@ -0,0 +1,21 @@
+export default abstract class CaffeineBeverageWithHook {
+  prepareRecipe(): void {
+    this.boilWater();
+    this.brew();
+    this.pourInCup();
+    if (this.customerWantsCondiments()) {
+      this.addCondiments();
+    }
+  }
+  boilWater(): void {
+    console.log("Boiling water");
+  }
+  pourInCup(): void {
+    console.log("Pouring into cup");
+  }
+  abstract brew(): void;
+  abstract addCondiments(): void;
+  customerWantsCondiments(): boolean {
+    return true;
+  }
+}
