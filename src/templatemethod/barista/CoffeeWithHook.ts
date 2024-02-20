78dd99d1d8531f4133e1e4834f78e7f8b06943d6
macluiggy <luiggy.macias@imagineapps.co>
2024-02-20 14:06:13 -0500

    Add CoffeeWithHook class to implement the template method pattern

---
+++ src/templatemethod/barista/CoffeeWithHook.ts
@@ -0,0 +1,13 @@
+import CaffeineBeverageWithHook from "./CaffeineBeverageWithHook";
+
+export default class CoffeeWithHook extends CaffeineBeverageWithHook {
+  brew(): void {
+    console.log("Dripping Coffee through filter");
+  }
+  addCondiments(): void {
+    console.log("Adding Sugar and Milk");
+  }
+  customerWantsCondiments(): boolean {
+    return window.confirm("Do you want milk and sugar?");
+  }
+}
