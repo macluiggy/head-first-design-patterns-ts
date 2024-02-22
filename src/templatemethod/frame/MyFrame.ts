aa5ba5d3886ef7560b675ded1caf0e90bfe19be6
macluiggy <luiggy.macias@imagineapps.co>
2024-02-22 11:54:08 -0500

    Update start script in package.json and extend MyFrame from BrowserWindow

--- src/templatemethod/frame/MyFrame.ts
+++ src/templatemethod/frame/MyFrame.ts
@@ -1,38 +1,20 @@
 import { app, BrowserWindow } from "electron";
 
-class MyFrame {
+class MyFrame extends BrowserWindow {
   private window: BrowserWindow | null;
 
   constructor(title: string) {
-    app.on("ready", () => {
-      this.window = new BrowserWindow({
-        width: 300,
-        height: 300,
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false,
-        },
-      });
-
-      this.window.loadURL(
-        `data:text/html, <body onload="document.body.innerText='I rule!!'"></body>`
-      );
-      this.window.on("closed", () => {
-        this.window = null;
-      });
+    super({
+      width: 300,
+      height: 300,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
     });
-
-    app.on("window-all-closed", () => {
-      if (process.platform !== "darwin") {
-        app.quit();
-      }
-    });
-
-    app.on("activate", () => {
-      if (this.window === null) {
-        this.createWindow();
-      }
-    });
+    this.window = null;
+    this.createWindow();
+    
   }
 
   createWindow() {
