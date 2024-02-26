a0c0294c4162488c5d4414b15eebb37109fb5e3d
macluiggy <luiggy.macias@imagineapps.co>
2024-02-26 14:45:28 -0500

    Add console logs and iterate over menu items

--- src/iterator/first/firstTest.ts
+++ src/iterator/first/firstTest.ts
@@ -4,6 +4,17 @@ import PancakeHouseMenu from "./PancakeHouseMenu";
 
 const pancakeHouseMenu = new PancakeHouseMenu();
 const breakfastItems = pancakeHouseMenu.getMenuItems();
+// console.log('Pancake House Menu', breakfastItems);
+
 
 const dinerMenu: DinerMenu = new DinerMenu();
 const lunchItems: MenuItem[] = dinerMenu.getMenuItems();
+// console.log('Diner Menu', lunchItems);
+
+for (const item of breakfastItems) {
+  console.log(item.toString());
+}
+
+for (const item of lunchItems) {
+  console.log(item.toString());
+}
