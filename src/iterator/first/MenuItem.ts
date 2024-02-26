b6bd75f03779d7264272db320e01f108733f28ad
macluiggy <luiggy.macias@imagineapps.co>
2024-02-26 14:29:10 -0500

    Add MenuItem class to iterator/first directory

---
+++ src/iterator/first/MenuItem.ts
@@ -0,0 +1,27 @@
+export default class MenuItem {
+  name: string;
+  description: string;
+  vegetarian: boolean;
+  price: number;
+  constructor(name: string, description: string, vegetarian: boolean, price: number) {
+    this.name = name;
+    this.description = description;
+    this.vegetarian = vegetarian;
+    this.price = price;
+  }
+  getName(): string {
+    return this.name;
+  }
+  getDescription(): string {
+    return this.description;
+  }
+  getPrice(): number {
+    return this.price;
+  }
+  isVegetarian(): boolean {
+    return this.vegetarian;
+  }
+  toString(): string {
+    return `${this.name}, ${this.price} -- ${this.description}`;
+  }
+}
