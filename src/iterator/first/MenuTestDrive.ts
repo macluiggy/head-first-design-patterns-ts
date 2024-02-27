6b4f04c10372ac3f9f5eaebc03efcee00d3b27fe
macluiggy <luiggy.macias@imagineapps.co>
2024-02-27 12:11:17 -0500

    Fix iterator class for DinerMenu

--- src/iterator/first/MenuTestDrive.ts
+++ src/iterator/first/MenuTestDrive.ts
@@ -6,7 +6,7 @@ import Waitress from "./Waitress";
 export default class MenuTestDrive {
   static main(): void {
     const pancakeHouseMenu = new PancakeHouseMenu();
-    const dinerMenu = new DinerMenu();
+    const dinerMenu = new DinerMenu(); // this is the only one that need a specific iterator class built in since it's an array, unlike the other two, pancakeHouseMenu and caffeMenu that are a set and a map, respectively
     const caffeMenu = new CaffeMenu();
     const waitress = new Waitress(pancakeHouseMenu, dinerMenu, caffeMenu);
     waitress.printMenu();
