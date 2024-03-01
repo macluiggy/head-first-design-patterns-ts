9579418665abddcb0dc3c2c550b6925712dc92e3
macluiggy <luiggy.macias@imagineapps.co>
2024-03-01 14:43:10 -0500

    Refactor Waitress class to accept an array of menus

--- src/iterator/first/MenuTestDrive.ts
+++ src/iterator/first/MenuTestDrive.ts
@@ -8,7 +8,7 @@ export default class MenuTestDrive {
     const pancakeHouseMenu = new PancakeHouseMenu();
     const dinerMenu = new DinerMenu(); // this is the only one that need a specific iterator class built in since it's an array, unlike the other two, pancakeHouseMenu and caffeMenu that are a set and a map, respectively
     const caffeMenu = new CaffeMenu();
-    const waitress = new Waitress(pancakeHouseMenu, dinerMenu, caffeMenu);
+    const waitress = new Waitress([pancakeHouseMenu, dinerMenu, caffeMenu]);
     waitress.printMenu();
   }
 }
--- src/iterator/first/Waitress.ts
+++ src/iterator/first/Waitress.ts
@@ -2,32 +2,40 @@ import Menu from "./Menu";
 import MenuItem from "./MenuItem";
 
 export default class Waitress {
-  pancakeHouseMenu: Menu;
-  dinerMenu: Menu;
-  cafeMenu: Menu;
+  // pancakeHouseMenu: Menu;
+  // dinerMenu: Menu;
+  // cafeMenu: Menu;
+  list: Menu[];
 
-  constructor(pancakeHouseMenu: Menu, dinerMenu: Menu, cafeMenu: Menu) {
-    this.pancakeHouseMenu = pancakeHouseMenu;
-    this.dinerMenu = dinerMenu;
-    this.cafeMenu = cafeMenu;
+  // constructor(pancakeHouseMenu: Menu, dinerMenu: Menu, cafeMenu: Menu) {
+  //   this.pancakeHouseMenu = pancakeHouseMenu;
+  //   this.dinerMenu = dinerMenu;
+  //   this.cafeMenu = cafeMenu;
+  // }
+  constructor(list: Menu[]) {
+    this.list = list;
   }
 
   printMenu(): void {
-    const pancakeIterator: Iterator<MenuItem> =
-      this.pancakeHouseMenu.createIterator();
-    const dinerIterator: Iterator<
-      MenuItem,
-      MenuItem,
-      MenuItem | undefined | null
-    > = this.dinerMenu.createIterator();
-    const cafeIterator: Iterator<MenuItem> = this.cafeMenu.createIterator();
+    // const pancakeIterator: Iterator<MenuItem> =
+    //   this.pancakeHouseMenu.createIterator();
+    // const dinerIterator: Iterator<
+    //   MenuItem,
+    //   MenuItem,
+    //   MenuItem | undefined | null
+    // > = this.dinerMenu.createIterator();
+    // const cafeIterator: Iterator<MenuItem> = this.cafeMenu.createIterator();
 
-    console.log("MENU\n----\nBREAKFAST");
-    this.printMenuItems(pancakeIterator);
-    console.log("\nLUNCH");
-    this.printMenuItems(dinerIterator);
-    console.log("\nDINNER");
-    this.printMenuItems(cafeIterator);
+    // console.log("MENU\n----\nBREAKFAST");
+    // this.printMenuItems(pancakeIterator);
+    // console.log("\nLUNCH");
+    // this.printMenuItems(dinerIterator);
+    // console.log("\nDINNER");
+    // this.printMenuItems(cafeIterator);
+    for (const menu of this.list) {
+      const iterator = menu.createIterator();
+      this.printMenuItems(iterator);
+    }
   }
 
   private printMenuItems(iterator: Iterator<MenuItem>): void {
