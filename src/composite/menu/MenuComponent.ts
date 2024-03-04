0a210298ced6f2713978de68bb1006ae2b71a04d
macluiggy <luiggy.macias@imagineapps.co>
2024-03-04 11:03:17 -0500

    Add MenuComponent class for composite pattern implementation

---
+++ src/composite/menu/MenuComponent.ts
@@ -0,0 +1,37 @@
+export default class MenuComponent {
+  add(menuComponent: MenuComponent): void {
+    throw new Error("Unsupported Operation");
+  }
+
+  remove(menuComponent: MenuComponent): void {
+    throw new Error("Unsupported Operation");
+  }
+
+  getChild(i: number): MenuComponent {
+    throw new Error("Unsupported Operation");
+  }
+
+  getName(): string {
+    throw new Error("Unsupported Operation");
+  }
+
+  getDescription(): string {
+    throw new Error("Unsupported Operation");
+  }
+
+  getPrice(): number {
+    throw new Error("Unsupported Operation");
+  }
+
+  isVegetarian(): boolean {
+    throw new Error("Unsupported Operation");
+  }
+
+  print(): void {
+    throw new Error("Unsupported Operation");
+  }
+
+  createIterator(): Iterator<MenuComponent> {
+    throw new Error("Unsupported Operation");
+  }
+}
