7fd6f69527873b889dc28430f85440833e44ff4e
macluiggy <luiggy.macias@imagineapps.co>
2024-03-05 10:59:19 -0500

    Add FordTestDrive class for testing Ford2024 and TeslaAdapter

---
+++ src/adapter/car/FordTestDrive.ts
@@ -0,0 +1,28 @@
+import Ford2024 from "./Ford2024";
+import FordAdapter from "./FordAdapter";
+import TeslaAdapter from "./TeslaAdapter";
+import TeslaXModel from "./TexlaXModel";
+
+export default class FordTestDrive {
+  static main(): void {
+    const ford2024 = new Ford2024();
+
+    console.log("The Ford 2024 says...");
+    FordTestDrive.testFord(ford2024);
+
+    const tesla = new TeslaXModel();
+    console.log("\nThe Ford 2024 adapted to a Tesla says...");
+    FordTestDrive.testFord(new TeslaAdapter(tesla));
+  }
+
+  static testFord(ford2024: Ford2024): void {
+    ford2024.startEngine();
+    ford2024.accelerate();
+    ford2024.brake();
+    ford2024.honk();
+    ford2024.stopEngine();
+  }
+}
+
+FordTestDrive.main();
+
