1fac28d99e73d977e72e0298dc291c762385d901
macluiggy <luiggy.macias@imagineapps.co>
2024-03-07 11:48:39 -0500

    Add GumballMachineTestDrive class for testing GumballMachine functionality

---
+++ src/state/gumballstate/GumballMachineTestDrive.ts
@@ -0,0 +1,40 @@
+import GumballMachine from "./GumballMachine";
+
+export default class GumballMachineTestDrive {
+  static main(): void {
+    const gumballMachine = new GumballMachine(5);
+
+    console.log(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    console.log(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.ejectQuarter();
+    gumballMachine.turnCrank();
+
+    console.log(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.ejectQuarter();
+
+    console.log(gumballMachine);
+
+    gumballMachine.insertQuarter();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+    gumballMachine.insertQuarter();
+    gumballMachine.turnCrank();
+
+    console.log(gumballMachine);
+  }
+}
+
+GumballMachineTestDrive.main();
