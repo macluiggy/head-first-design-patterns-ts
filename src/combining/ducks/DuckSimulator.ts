6a783aaf47486b512e238864117695ca7ca1a090
macluiggy <luiggy.macias@imagineapps.co>
2024-03-11 12:16:58 -0500

    Add DuckSimulator class and main method

---
+++ src/combining/ducks/DuckSimulator.ts
@@ -0,0 +1,33 @@
+import DuckCall from "./DuckCall";
+import MallardDuck from "./MallardDuck";
+import Quackable from "./Quackable";
+import RedheadDuck from "./RedheadDuck";
+import RubberDuck from "./RubberDuck";
+
+export default class DuckSimulator {
+  public static main(): void {
+    const simulator: DuckSimulator = new DuckSimulator();
+    simulator.simulate();
+  }
+
+  public simulate(): void {
+    const mallardDuck: Quackable = new MallardDuck();
+    const redheadDuck: Quackable = new RedheadDuck();
+    const duckCall: Quackable = new DuckCall();
+    const rubberDuck: Quackable = new RubberDuck();
+    
+    console.log('\nDuck Simulator');
+
+    this._simulate(mallardDuck);
+    this._simulate(redheadDuck);
+    this._simulate(duckCall);
+    this._simulate(rubberDuck);
+
+  }
+
+  private _simulate(duck: Quackable): void {
+    duck.quack();
+  }
+}
+
+DuckSimulator.main();
