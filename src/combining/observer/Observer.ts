7202a1b78e5a3cdc4419f3a42d2c219488eb9d55
macluiggy <luiggy.macias@imagineapps.co>
2024-03-11 20:39:41 -0500

    Refactor Observer class to use an interface

--- src/combining/observer/Observer.ts
+++ src/combining/observer/Observer.ts
@@ -1,7 +1,5 @@
 import QuackObservable from "./QuackObservable";
 
-export default class Observer {
-  update(duck: QuackObservable): void {
-    console.log('Quackologist: ' + duck + ' just quacked.');
-  }
+export default interface Observer {
+  update(duck: QuackObservable): void;
 }
