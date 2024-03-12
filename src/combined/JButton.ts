4de2ce43437b1751b6927859f66b0d5a0dd86fcf
macluiggy <luiggy.macias@imagineapps.co>
2024-03-12 15:09:48 -0500

    Add JButton, JMenu, JMenuBar, and JMenuItem classes

---
+++ src/combined/JButton.ts
@@ -0,0 +1,12 @@
+export default class JButton {
+  model: any;
+  constructor() {
+    this.model = null;
+  }
+  setText(text: string) {
+    // Not implemented
+  }
+  setValue(value: number) {
+    // Not implemented
+  }
+}
---
+++ src/combined/JMenuBar.ts
@@ -0,0 +1,12 @@
+export default class JMenuBar {
+  model: any;
+  constructor() {
+    this.model = null;
+  }
+  setText(text: string) {
+    // Not implemented
+  }
+  setValue(value: number) {
+    // Not implemented
+  }
+}
