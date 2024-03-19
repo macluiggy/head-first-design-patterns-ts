76a778cdd76fc7726226b6f1a68ced40105f80a6
macluiggy <luiggy.macias@imagineapps.co>
2024-03-19 12:32:15 -0500

    Add ControllerInterface implementation to HeartController

--- src/combined/HeartController.ts
+++ src/combined/HeartController.ts
@@ -1,1 +1,19 @@
-export default class HeartController {}
+import ControllerInterface from "./ControllerInterface";
+
+export default class HeartController implements ControllerInterface {
+  start(): void {
+    throw new Error("Method not implemented.");
+  }
+  stop(): void {
+    throw new Error("Method not implemented.");
+  }
+  increaseBPM(): void {
+    throw new Error("Method not implemented.");
+  }
+  decreaseBPM(): void {
+    throw new Error("Method not implemented.");
+  }
+  setBPM(bpm: number): void {
+    throw new Error("Method not implemented.");
+  }
+}
