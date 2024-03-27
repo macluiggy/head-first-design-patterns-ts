c773f9f8eadc886a5175472f5f3bacc037d0d9ff
macluiggy <luiggy.macias@imagineapps.co>
2024-03-27 17:18:52 -0500

    Add door lock and unlock functionality to DoorTestDrive

--- src/state/door/DoorTestDrive.ts
+++ src/state/door/DoorTestDrive.ts
@@ -1,5 +1,14 @@
+
+import Door from "./Door";
 export default class DoorTestDrive {
-  public static main(): void {}
+  public static main(): void {
+    const door = new Door();
+    door.lock();
+    door.unlock();
+    door.unlock();
+    door.lock();
+    door.lock();
+  }
 }
 
 DoorTestDrive.main();
