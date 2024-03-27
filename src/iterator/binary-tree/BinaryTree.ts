0b97c218bc0467207d0cdfdd39ae5b767dba9fbd
macluiggy <luiggy.macias@imagineapps.co>
2024-03-26 19:35:09 -0500

    Add method to BinaryTree class and update BinaryTreeTestDrive to test iterator

--- src/iterator/binary-tree/BinaryTree.ts
+++ src/iterator/binary-tree/BinaryTree.ts
@@ -9,6 +9,31 @@ export default class BinaryTree<T> {
     this.root = null;
   }
 
+  add(value: T): void {
+    const node = new TreeNode(value);
+    if (!this.root) {
+      this.root = node;
+      return;
+    }
+
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = node;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = node;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+
   public getRoot(): TreeNode<T> | null {
     return this.root;
   }
--- src/iterator/binary-tree/BinaryTreeTestDrive.ts
+++ src/iterator/binary-tree/BinaryTreeTestDrive.ts
@@ -3,12 +3,17 @@ import BinaryTree from "./BinaryTree";
 export default class BinaryTreeTest {
   public static main(): void {
     const binaryTree = new BinaryTree<number>();
+    binaryTree.add(1);
+    binaryTree.add(2);
     binaryTree.getRoot();
     const iterator = binaryTree.createIterator();
-    console.log(iterator);
+    // console.log(iterator);
     
-    iterator.hasNext();
-    iterator.next();
+   
+    while (iterator.hasNext()) {
+      // iterator.next();
+      console.log(iterator.next());
+    }
   }
 }
 
