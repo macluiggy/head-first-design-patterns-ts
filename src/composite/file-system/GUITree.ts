b3e5c8774b9dd54a972b8cbfde72039da3d4f1c1
macluiggy <luiggy.macias@imagineapps.co>
2024-03-26 20:13:46 -0500

    Add GUITree class with methods to add files and directories, find directories, and print tree

---
+++ src/composite/file-system/GUITree.ts
@@ -0,0 +1,63 @@
+import File from "./File";
+import Directory from "./Directory";
+import FileSystemItem from "./FileSystemItem";
+
+export default class GUITree {
+  private root: Directory;
+
+  constructor() {
+    this.root = new Directory("Root");
+  }
+
+  addFile(name: string, size: number, directoryName: string = "Root"): void {
+    const directory = this.findDirectory(directoryName);
+    if (directory) {
+      directory.add(new File(name, size));
+    } else {
+      console.log(`Directory ${directoryName} not found`);
+    }
+  }
+
+  addDirectory(name: string, directoryName: string = "Root"): void {
+    const directory = this.findDirectory(directoryName);
+    if (directory) {
+      directory.add(new Directory(name));
+    } else {
+      console.log(`Directory ${directoryName} not found`);
+    }
+  }
+
+  findDirectory(
+    name: string,
+    directory: Directory = this.root
+  ): Directory | null {
+    if (directory.getName() === name) {
+      return directory;
+    }
+    for (const child of directory["children"]) {
+      if (child instanceof Directory) {
+        const found = this.findDirectory(name, child);
+        if (found) {
+          return found;
+        }
+      }
+    }
+    return null;
+  }
+
+  printTree(): void {
+    this.root.print();
+  }
+
+  getName(): string {
+    return this.root.getName();
+  }
+}
+
+const guiTree = new GUITree();
+guiTree.addFile("file1.txt", 100);
+guiTree.addDirectory("subFolder");
+guiTree.addFile("file2.txt", 200, "subFolder");
+guiTree.addFile("file3.txt", 150, "subFolder");
+
+guiTree.printTree();
