e76d478885b75283a2acefb63abe45ba2639e3f8
macluiggy <luiggy.macias@imagineapps.co>
2024-04-01 11:15:16 -0500

    Add MP3Player and WAVPlayer classes

---
+++ src/compound/adapter+facade/media-player/MP3Player.ts
@@ -0,0 +1,13 @@
+export default class MP3Player {
+  startPlayback(): void {
+    console.log("Playing MP3 file");
+  }
+
+  pauseMp3(): void {
+    console.log("Pausing MP3 file");
+  }
+
+  stopMP3(): void {
+    console.log("Stopping MP3 file");
+  }
+}
---
+++ src/compound/adapter+facade/media-player/WAVPlayer.ts
@@ -0,0 +1,13 @@
+export default class WAVPlayer {
+  playWAV(): void {
+    console.log("Playing WAV file");
+  }
+
+  pauseWAV(): void {
+    console.log("Pausing WAV file");
+  }
+
+  stopWAV(): void {
+    console.log("Stopping WAV file");
+  }
+}
