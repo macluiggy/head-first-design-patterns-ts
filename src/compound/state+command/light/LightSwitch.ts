90034cc7268d4ff4ce28a70168017cfc225e2de1
macluiggy <luiggy.macias@imagineapps.co>
2024-04-01 12:26:42 -0500

    Add LightOffCommand and Light import to LightSwitch class

--- src/compound/state+command/light/LightSwitch.ts
+++ src/compound/state+command/light/LightSwitch.ts
@@ -1,7 +1,14 @@
 import Command from "./Command";
+import Light from "./Light";
+import LightOffCommand from "./LightOffCommand";
+
 
 export default class LightSwitch {
   command: Command;
+
+  constructor() {
+    this.command = new LightOffCommand(new Light());
+  }
 
   setCommand(command: Command): void {
     this.command = command;
