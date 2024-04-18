d01d14b69f440ca8869d97aeb3b4925a198d3ec5
macluiggy <luiggy.macias@imagineapps.co>
2024-04-17 20:42:28 -0500

    vfd

--- src/composite/file-system/File.ts
+++ src/composite/file-system/File.ts
@@ -26,3 +26,10 @@ export default class File implements FileSystemItem {
     return this.size;
   }
 }
+
+// gaste mas de 400 latas para que al final la vista se me siga desviando, maldita sea, que porqueria de vista
+// parece que no se va a arreglar, que porqueria de vista, no se va a arreglar, que porqueria de vista, no se va a arreglar
+// es molesto este ojo derecho, no tengo control como el izquierdo, no tengo control como el izquierdo, no tengo control como el izquierdo
+// en serio que estoy arto de entonces pague dinero por gusto, ya que me sigue molestando esta vista de mierda, que porqueria de vista
+// que puedo hacer, no tengo dinero para pagar otra operacion
+// mi unica esperanza es que se arregle solo, pero no creo que se arregle solo, no creo que se arregle solo, no creo que se arregle solo
